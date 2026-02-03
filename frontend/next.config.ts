3ef075949b1dc629bc9d7891c5866364bfb58f01
canadaell <mingdezhou1@outlook.com>
2026-02-03 16:37:19 +0000

    cicd with docker compose

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    // If you use external images (like from R2), you might need to whitelist domains here later
+    // domains: ['your-r2-domain.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
